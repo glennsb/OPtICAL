b8eb85491427138a99208d4b502bbfc80b0ec65e
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-09-29 14:17:07 -0500

    Update list of sub module/classes, main driver bin starts the cli

--- lib/optical.rb
+++ lib/optical.rb
@@ -1,5 +1,12 @@
+# Copyright (c) 2014, Stuart Glenn, OMRF
+# Distributed under a BSD 3-Clause
+# Full license available in LICENSE.txt distributed with this software
+
 require "optical/version"
+require "optical/configuration"
+require "optical/library"
+require "optical/sample"
+require "optical/chip_analysis"
 
 module Optical
-  # Your code goes here...
 end
