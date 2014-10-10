50d89bcbfea886eaab47f2513ecbbc6df532d784
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-10-10 13:42:52 -0500

    Fix full class path bug for filter

--- lib/optical/filters/at_least_one_end_unique.rb
+++ lib/optical/filters/at_least_one_end_unique.rb
@@ -6,7 +6,7 @@ class Optical::Filters::AtLeastOneEndUnique < Optical::Filters::NullFilter
   def filter_to(output_bam)
     if ! @lib.is_paired?
       # A single end with at least one end unique, is really just only unique, duh
-      OnlyUnique.new(@lib,@name,@conf).filter_to(output_bam)
+      Optical::Filters::OnlyUnique.new(@lib,@name,@conf).filter_to(output_bam)
     else
       filter_through_awk_script([File.join(File.dirname(__FILE__),"paired_end_filters.awk"),"at_least_one_unique"],
                                   output_bam,@conf.min_map_quality_score,true)
