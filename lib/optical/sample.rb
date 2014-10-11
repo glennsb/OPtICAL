1ecea4060d5ad682bd78b44dc3c136dca7e7774f
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-10-10 21:24:19 -0500

    Add method to turn a sample into a pair of PR samples

--- lib/optical/sample.rb
+++ lib/optical/sample.rb
@@ -30,4 +30,28 @@ class Optical::Sample
   def to_s
     @name
   end
+
+  def create_pseudo_replicates(num_replicates,output_base,conf)
+    return nil unless analysis_ready_bam && File.exists?(analysis_ready_bam.path)
+    return nil unless Dir.exists?(output_base)
+    # shuffle the bam, and ever %num_replicates goes to different file, then sort those
+    rep_base_name = "#{safe_name}_pseudo_replicate"
+    outname = File.join(output_base,rep_base_name)
+    cmd = conf.cluster_cmd_prefix(free:10, max:40, sync:true, name:"replicate_#{safe_name}") +
+          %W(optical pseudoReplicateBam -b #{analysis_ready_bam.path} -o #{outname} -r #{num_replicates})
+    unless system(*cmd)
+      return nil
+    end
+    replicate_samples = []
+    num_replicates.times do |r|
+      rep = (r+1).to_s.rjust(2,"0")
+      path = "#{outname}_#{rep}}"
+      b = Optical::Bam.new(path,has_paired?())
+      b.fragment_size = analysis_ready_bam.fragment_size()
+      rep = Optical::Sample.new("#{rep_base_name}_#{rep}",[])
+      rep.analysis_ready_bam=b
+      replicate_samples << rep
+    end
+    return replicate_samples
+  end
 end
