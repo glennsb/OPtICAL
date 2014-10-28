d74d7addaa997daf72df1bdf0f0844d6472b9a7e
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-10-28 15:52:49 -0500

    Add method to read in the _alignment_qc.txt data
    Let the library know how to read in its own data file & put them in a
    handy hash for use down the road in the report

--- lib/optical/library.rb
+++ lib/optical/library.rb
@@ -20,4 +20,26 @@ class Optical::Library
   def add_fastqc_path(path)
     @fastqc_paths << path
   end
+
+  def load_stats()
+    IO.foreach(@qc_path) do |line|
+      next if 0 == $.
+      (name,number_unique_mapped_reads,number_multiple_hit_reads,
+       number_unmapped_reads,number_total_reads,number_total_mapped_reads,
+       number_genomic_positions_with_single_unique_read_mapped,
+       number_genomic_positions_with_greater_than_one_unique_read_mapped,
+       number_total_genomic_positions_with_unique_read_mapped) = line.chomp.split(/\t/)
+      @name = name
+      @mapping_counts = {
+        "number unique mapped reads" => number_unique_mapped_reads.to_i,
+        "number multiple hit reads" => number_multiple_hit_reads.to_i,
+        "number unmapped reads" => number_unmapped_reads.to_i,
+        "number total reads" => number_total_reads.to_i,
+        "number total mapped reads" => number_total_mapped_reads.to_i,
+        "number genomic positions with single unique read mapped" => number_genomic_positions_with_single_unique_read_mapped.to_i,
+        "number genomic positions with greater than one unique read mapped" => number_genomic_positions_with_greater_than_one_unique_read_mapped.to_i,
+        "number total genomic positions with unique read mapped" => number_total_genomic_positions_with_unique_read_mapped.to_i
+      }
+    end
+  end
 end
