ab6b3b6e089d9ca2d93a6cb6dc448dc3e6e14c61
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-10-28 15:55:25 -0500

    Change for wordy strings to symbols

--- lib/optical/library.rb
+++ lib/optical/library.rb
@@ -31,14 +31,14 @@ class Optical::Library
        number_total_genomic_positions_with_unique_read_mapped) = line.chomp.split(/\t/)
       @name = name
       @mapping_counts = {
-        "number unique mapped reads" => number_unique_mapped_reads.to_i,
-        "number multiple hit reads" => number_multiple_hit_reads.to_i,
-        "number unmapped reads" => number_unmapped_reads.to_i,
-        "number total reads" => number_total_reads.to_i,
-        "number total mapped reads" => number_total_mapped_reads.to_i,
-        "number genomic positions with single unique read mapped" => number_genomic_positions_with_single_unique_read_mapped.to_i,
-        "number genomic positions with greater than one unique read mapped" => number_genomic_positions_with_greater_than_one_unique_read_mapped.to_i,
-        "number total genomic positions with unique read mapped" => number_total_genomic_positions_with_unique_read_mapped.to_i
+        unique_mapped_reads:number_unique_mapped_reads.to_i,
+        multiple_hit_reads:number_multiple_hit_reads.to_i,
+        unmapped_reads:number_unmapped_reads.to_i,
+        total_reads:number_total_reads.to_i,
+        total_mapped_reads:number_total_mapped_reads.to_i,
+        genomic_positions_with_single_unique_read_mapped:number_genomic_positions_with_single_unique_read_mapped.to_i,
+        genomic_positions_with_greater_than_one_unique_read_mapped:number_genomic_positions_with_greater_than_one_unique_read_mapped.to_i,
+        total_genomic_positions_with_unique_read_mapped:number_total_genomic_positions_with_unique_read_mapped.to_i
       }
     end
   end
