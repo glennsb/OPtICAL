67c32b1e368909a8424a1f7d34f57c0a3c7f8db9
Stuart Glenn <Stuart-Glenn@omrf.org>
2014-11-07 11:16:14 -0600

    Bump version number, since we are almost useful

--- lib/optical/version.rb
+++ lib/optical/version.rb
@@ -3,6 +3,6 @@
 # Full license available in LICENSE.txt distributed with this software
 
 module Optical
-  VERSION = "0.0.1"
-  RELEASEDATE = "2014-09-22"
+  VERSION = "0.1.0"
+  RELEASEDATE = "2014-11-07"
 end
