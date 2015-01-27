8d4f77566fe2760ef7424b64aebe976186e3c5b5
Stuart Glenn <Stuart-Glenn@omrf.org>
2015-01-27 10:55:46 -0600

    Sort listing of to be merged samples in peak callers

--- lib/optical/configuration.rb
+++ lib/optical/configuration.rb
@@ -18,11 +18,11 @@ class Optical::Configuration
     confy['peak_callers'].each do |caller_name,settings|
       raise "No settings for caller #{caller_name}" unless settings
       settings[:comparisons].each do |comp|
-        treatments = comp[:treatments].split(/\s+/).map do |s|
+        treatments = comp[:treatments].split(/\s+/).sort.map do |s|
           raise "Invalid sample #{s} in treatment section for #{caller_name}" unless samples.has_key?(s)
           samples[s]
         end
-        controls = (comp[:controls]||"").split(/\s+/).map do |s|
+        controls = (comp[:controls]||"").split(/\s+/).sort.map do |s|
           raise "Invalid sample #{s} in control section for #{caller_name}" unless samples.has_key?(s)
           samples[s]
         end
